7449442cfcc0b53ce963cc805527c7ed4250f83e
Logan Bertram <bertralo@oregonstate.edu>
2018-11-04 21:52:02 -0800

    Text box and JS update

---
+++ main.js
@@ -0,0 +1,5 @@
+$ (updateView)
+
+function updateView() {
+    alert("I asked my wife what I should put in this text box, and her suggestion was not very appropriate. So I decided to just put this instead.")
+}

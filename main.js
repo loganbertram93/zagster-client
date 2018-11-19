68e08896e8b8d40e8997ab676aed1667bf501007
Logan Bertram <bertralo@oregonstate.edu>
2018-11-19 13:13:46 -0800

    Added Graph and manually entered some simple date

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
-$ (updateView)
+$(updateView)
 
 function updateView() {
     $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+    $.getJSON(BASE_URL + "/rides/count/per_month" , updateMonthlyRides)
 }
-
 function updateRideCount(data) {
     numberOfRides = data.count
     $("h2#rideCount").html(numberOfRides)
+}
+
+function updateMonthlyRides(data) {
+console.log(data)
 }
